f4b14e57315bcd304c3d7daeae57f26c0839dd6b
Jagadeesh Kotra <jagadeesh@stdin.top>
2025-01-22 02:35:06 +0530

    add wasm pkg for pgp verify

---
+++ content/pgp/pgp-verify-wasm/pkg/pgp_verify_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_pgpdetails_free: (a: number, b: number) => void;
+export const __wbg_get_pgpdetails_expiration: (a: number) => bigint;
+export const __wbg_set_pgpdetails_expiration: (a: number, b: bigint) => void;
+export const pgpdetails_id: (a: number) => [number, number];
+export const verify_key: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
